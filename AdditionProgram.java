519d5878d6b1c96671d2fe887c18c37026e94a31
Raghavendrajoshi45 <74344432+Raghavendrajoshi45@users.noreply.github.com>
2025-09-26 11:09:48 +0530

    Update AdditionProgram.java

--- AdditionProgram.java
+++ AdditionProgram.java
@@ -1,13 +1,6 @@
 public class AdditionProgram {
     public static void main(String[] args) {
        
-        int number1 = 15;
-        int number2 = 25;
-
-
-        int sum = number1 + number2;
-
-       
-        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+       System.out.println("HelloWorld");
     }
 }
