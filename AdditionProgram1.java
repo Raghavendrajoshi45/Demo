2931c2b4eec41c833db36b4f075d87930b47b075
Raghavendrajoshi45 <74344432+Raghavendrajoshi45@users.noreply.github.com>
2025-10-03 20:33:50 +0530

    Create AdditionProgram1.java

---
+++ AdditionProgram1.java
@@ -0,0 +1,9 @@
+public class AdditionProgram1 {
+    public static void main(String[] args) {
+       
+       System.out.println("HelloWorld");
+        System.out.println("Hiii");
+         System.out.println("Good Morning");
+        
+    }
+}
